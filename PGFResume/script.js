e7171967f1c95bc9cbb2bdf4ab765ff6932dc715
Pedro Fernandes <pedro92.gf@gmail.com>
2017-09-17 13:01:12 -0400

    update project.

--- PGFResume/script.js
+++ PGFResume/script.js
@@ -2,6 +2,22 @@ $(function() {
     $('.owl-carousel').owlCarousel({
         singleItem: true,
         items: 4,
-        margin: 0
+        margin: 0,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:false
+            },
+            1000:{
+                items:4,
+                nav:true,
+                loop:false
+            }
+        }
     });
 });
