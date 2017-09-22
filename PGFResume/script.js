bfb2e10ab75f38c240d58c16d438deda706feebb
Pedro Fernandes <pedro92.gf@gmail.com>
2017-09-22 19:36:34 -0400

    resume project.

--- PGFResume/script.js
+++ PGFResume/script.js
@@ -1,23 +1,24 @@
 $(function() {
-    $('.owl-carousel').owlCarousel({
-        singleItem: true,
+  // Connect owl carousel to the project. https://owlcarousel2.github.io/OwlCarousel2/
+  $('.owl-carousel').owlCarousel({
+    singleItem: true,
+    items: 4,
+    margin: 0,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      600: {
+        items: 3,
+        nav: false
+      },
+      1000: {
         items: 4,
-        margin: 0,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:true
-            },
-            600:{
-                items:3,
-                nav:false
-            },
-            1000:{
-                items:4,
-                nav:true,
-                loop:false
-            }
-        }
-    });
+        nav: true,
+        loop: false
+      }
+    }
+  });
 });
