61cbd004e4764a97dc8b75389518c57da2758a5d
Pedro Fernandes <pedro92.gf@gmail.com>
2017-09-24 13:25:20 -0400

    contact me form

--- PGFResume/script.js
+++ PGFResume/script.js
@@ -21,4 +21,45 @@ $(function() {
       }
     }
   });
+
+  $('#form-contact').submit(function() {
+    var inputEmail = $("#input-email");
+    var inputName = $("#input-name");
+    var inputMessage = $("#input-message");
+
+
+    var hasError;
+
+    var isNameEmpty = inputName.val().length == 0;
+    if (isNameEmpty) {
+      inputName.parent().addClass('has-error');
+      hasError = true;
+    } else {
+      inputName.parent().removeClass('has-error');
+    }
+
+    if (!validateEmail(inputEmail.val())) {
+      inputEmail.parent().addClass('has-error');
+      hasError = true;
+    } else {
+      inputEmail.parent().removeClass('has-error');
+    }
+
+    var isMessageEmpty = inputMessage.val().length == 0;
+    if (isMessageEmpty) {
+      inputMessage.parent().addClass('has-error');
+      hasError = true;
+    } else {
+      inputMessage.parent().removeClass('has-error');
+    }
+
+    return !hasError;
+
+  });
+
+
+  function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
 });
